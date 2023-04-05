ca635875ff709af6259ff556b166020510176d88
Mohit Gangadiya <mohit.g@esparkinfo.com>
2023-04-05 19:20:16 +0530

    change env example

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -1,14 +1,5 @@
 const axios = require("axios");
-const {
-  HUBSPOT_API_URL,
-  HUBSPOT_PRIVATE_APP_TOKEN,
-  PAYMENT_URL,
-  CLICKUP_API_URL,
-  CLICKUP_API_TOKEN,
-} = require("../config/env.config");
-const { logger } = require("../config/logger.config");
-// const { sleep } = require("../helpers/utils");
-// const { SF_URL } = require("./env.config");
+const { CLICKUP_API_URL, CLICKUP_API_TOKEN } = require("../config/env.config");
 
 const axiosClickUp = axios.create({
   baseURL: CLICKUP_API_URL,
--- src/config/env.config.js
+++ src/config/env.config.js
@@ -12,10 +12,6 @@ const envObject = {
   PORT: Number(process.env.PORT),
   BASE_URL: process.env.BASE_URL,
   WELCOME_MESSAGE: process.env.WELCOME_MESSAGE,
-  ENDPOINTSECRET: process.env.ENDPOINTSECRET,
-  STRIPE_API_KEY: process.env.STRIPE_API_KEY,
-  HUBSPOT_PRIVATE_APP_TOKEN: process.env.HUBSPOT_PRIVATE_APP_TOKEN,
-  HUBSPOT_API_URL: process.env.HUBSPOT_API_URL,
   CLICKUP_API_URL: process.env.CLICKUP_API_URL,
   CLICKUP_API_TOKEN: process.env.CLICKUP_API_TOKEN,
 };
